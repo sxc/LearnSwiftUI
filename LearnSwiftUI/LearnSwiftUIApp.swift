a543ce4e4058520f0af55fb85c03cc1459b92fb8
sxc <qiuyequ@icloud.com>
2022-11-07 09:28:33 +0800

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Xiaochun Shen on 2022/11/7.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
